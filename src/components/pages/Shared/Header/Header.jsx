e99eea7d89e394f3438ddf2e6f28dab36ab6aa88
asifahmed123 <ahmedasif092@gmail.com>
2023-05-05 16:30:23 +0600

    updated header

--- src/components/pages/Shared/Header/Header.jsx
+++ src/components/pages/Shared/Header/Header.jsx
@@ -58,7 +58,7 @@ const Header = () => {
                     <Navbar.Collapse>
                          <ActiveLink to='/'><span className='lg:text-2xl'>Home</span></ActiveLink>
                          <ActiveLink to='/blog'><span className='lg:text-2xl'>Blog</span></ActiveLink>
-                         <ActiveLink to='/register'><span className='lg:text-2xl'>Register</span></ActiveLink>
+                         <ActiveLink to='/login'><span className='lg:text-2xl'>Login</span></ActiveLink>
                     </Navbar.Collapse>
                </Navbar>
                <hr className='border-2'/>
